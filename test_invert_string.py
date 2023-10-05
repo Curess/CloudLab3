91e4ee2dfb0965de8b8eb85bf82c42eb83f32ed4
Curess <denisgodovannyi@gmail.com>
2023-10-05 18:30:08 +0300

    Update test_invert_string.py

--- test_invert_string.py
+++ test_invert_string.py
@@ -1,7 +1,8 @@
 # test_invert_string.py
 import pytest
-from invert_string import invert_string
+from invert_string import invert  # змінено з invert_string на invert
 
-def test_invert_string():
-    assert invert_string("hello") == "olleh"
-    assert invert_string("Привіт") == "тівирП"
+def test_invert():
+    assert invert("hello") == "olleh"  # змінено з invert_string на invert
+    assert invert("Привіт") == "тівирП"  # змінено з invert_string на invert
+
