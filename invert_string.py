5d0b8f780bfa461d05ee4b1bc323207c3c66f4d3
Curess <denisgodovannyi@gmail.com>
2023-10-05 18:29:15 +0300

    Create invert_string.py

---
+++ invert_string.py
@@ -0,0 +1,7 @@
+def invert(s: str) -> str:
+    return s[::-1]
+
+# Тестування функції
+original_string = "Привіт, світ!"
+inverted_string = invert(original_string)
+print(inverted_string)  # !тівс ,тівирП
