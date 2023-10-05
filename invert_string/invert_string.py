633e28aae1f6be3e638abf2ed3cbb354cfa3c804
Curess <denisgodovannyi@gmail.com>
2023-10-05 18:28:29 +0300

    Delete invert_string/invert_string.py

--- invert_string/invert_string.py
+++
@@ -1,7 +0,0 @@
-def invert_string(s: str) -> str:
-    return s[::-1]
-
-# Тестування функції
-original_string = "Привіт, світ!"
-inverted_string = invert_string(original_string)
-print(inverted_string)  # !тівс ,тівирП
